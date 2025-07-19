2dca189662de3a862d1cc9c371a534f503bd1829
raziqicha <mkrazique@gmail.com>
2025-07-19 08:53:29 +0400

    optimizing

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,6 +26,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#0d021a" />
+
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
         <Navbar />
