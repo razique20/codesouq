b30cbc76c98fd0819559cc636e59716d0d85623c
raziqicha <mkrazique@gmail.com>
2025-07-23 13:14:41 +0400

    button for pricing added

--- src/components/ui/SparkleReview.jsx
+++ src/components/ui/SparkleReview.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 import { SparklesCore } from "./HeroSparkle";
 
@@ -6,15 +7,24 @@ export function SparklesPreview() {
   return (
     <div className="relative min-h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md px-4">
       {/* Subheading */}
-     
 
       {/* Title */}
       <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-white via-violet-400 to-violet-900 z-20 drop-shadow-md">
         Algorithinn
       </h1>
-       <p className="text-center text-md  md:text-xl text-white/80 mb-4 z-20">
-        Crafting algorithms for <span className="text-violet-400">exponential business growth</span>
+      <p className="text-center text-md  md:text-xl text-white/80 mb-4 z-20">
+        Crafting algorithms for{" "}
+        <span className="text-violet-400">exponential business growth</span>
       </p>
+
+      <Link href="/pricing">
+        <button className="p-[3px] relative">
+          <div className="absolute inset-0 bg-gradient-to-r from-violet-500 to-violet-500 rounded-lg" />
+          <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+            Explore Plans
+          </div>
+        </button>
+      </Link>
 
       {/* Sparkle Area */}
       <div className="relative w-full max-w-3xl h-40 mt-6">
